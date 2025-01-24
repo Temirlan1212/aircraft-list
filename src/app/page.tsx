229037276527206ebe6ec65e3f43733d62a37c6b
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2025-01-24 20:20:28 +0600

    add pages layer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,45 @@
+"use client";
+import { dateUtils } from "@/shared/utils/date";
 import styles from "./page.module.css";
+import { aircraftStatusHistory } from "@/entities/aicraft-status-history/api";
 
 export default function Home() {
-  return <div className={styles.page}></div>;
+  const { data: aircrafts } =
+    aircraftStatusHistory.useGetAircraftsStatusHistoryQuery();
+  const [addAircraft] =
+    aircraftStatusHistory.useAddAircraftStatusHistoryMutation();
+  const [patchAircraft] =
+    aircraftStatusHistory.usePatchAircraftStatusHistoryMutation();
+
+  return (
+    <div className={styles.page}>
+      {/* <button
+        onClick={() => {
+          addAircraft({
+            value: "new status",
+            label: "new status",
+            color: "d",
+          });
+        }}
+      >
+        add
+      </button> */}
+
+      <button
+        onClick={() => {
+          patchAircraft({
+            comment: "new",
+            id: "1",
+          });
+        }}
+      >
+        patch
+      </button>
+      <div>
+        {aircrafts?.map((item) => {
+          return <div key={item.id}>{item.comment}</div>;
+        })}
+      </div>
+    </div>
+  );
 }
