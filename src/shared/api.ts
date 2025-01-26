439ce90ec266a833f68809cfceb6c17c06a94249
Temirlan Kadyrov <ltemirlan2003@gmail.com>
2025-01-26 15:22:30 +0600

    hotfix create bug after migrate json-server to older version

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -3,7 +3,10 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseUrl = "http://localhost:8000";
 
 export const baseApi = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl }),
+  baseQuery: fetchBaseQuery({
+    baseUrl,
+    headers: { "Content-Type": "application/json" },
+  }),
   tagTypes: ["Aircrafts", "Statuses", "AircraftStatusHistory"],
   endpoints: () => ({}),
 });
